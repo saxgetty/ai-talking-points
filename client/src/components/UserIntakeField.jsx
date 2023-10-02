976380123f49cd614cd80c8d3ff697f70632e82a
Sami <saxgetty4@gmail.com>
2023-10-02 02:21:41 -0700

    Setup new foundation for transformer ai model use

--- client/src/components/UserIntakeField.jsx
+++ client/src/components/UserIntakeField.jsx
@@ -1,52 +1,100 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const apiUrl = 'http://localhost:8000';
+const UserIntake = () => {
+  const apiUrl = 'http://localhost:8000/';
+  const [inputText, setInputText] = useState('');
+  const [generatedText, setGeneratedText] = useState('');
+  const [error, setError] = useState('');
 
-function UserIntake() {
-  const [userInput, setUserInput] = useState('');
-  const [aiResponse, setAiResponse] = useState(null);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const handleAiResponse = async () => {
     try {
-      const response = await fetch(`${apiUrl}/generateTalkingPoints/`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ input_text: userInput }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        setAiResponse(data.aiResponse);
-      } else {
-        console.error('Error:', response.status, response.statusText);
+      if (inputText.trim() === '') {
+        setError('Input cannot be empty');
+        return;
       }
+
+      console.log(inputText);
+
+      const response = await axios.post(`${apiUrl}generateTalkingPoints/`, { user_input: inputText }, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      setGeneratedText(response.data.generated_text);
+      setError('');
     } catch (error) {
       console.error('Error:', error);
+      setError('An error occurred while fetching the response.');
     }
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      {aiResponse && aiResponse.choices && aiResponse.choices.length > 0 && (
+      <input type="text" value={inputText} onChange={(e) => setInputText(e.target.value)} />
+      <button onClick={handleAiResponse}>Submit</button>
+      {error && <div>{error}</div>}
+      <div>
         <div>
-          <h2>AI Response:</h2>
-          <p>{aiResponse.choices[0].content}</p> {/* Display the content of the first choice */}
+          {inputText}
         </div>
-      )}
+        <div>{generatedText}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default UserIntake;
+
+// import React, { useState } from "react";
+
+// const apiUrl = 'http://localhost:8000';
+
+// function UserIntake() {
+//   const [userInput, setUserInput] = useState('');
+//   const [aiResponse, setAiResponse] = useState(null);
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await fetch(`${apiUrl}/generateTalkingPoints/`, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ input_text: userInput }),
+//       });
+
+//       if (response.ok) {
+//         const data = await response.json();
+//         setAiResponse(data.aiResponse);
+//       } else {
+//         console.error('Error:', response.status, response.statusText);
+//       }
+//     } catch (error) {
+//       console.error('Error:', error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           value={userInput}
+//           onChange={(e) => setUserInput(e.target.value)}
+//         />
+//         <button type="submit">Submit</button>
+//       </form>
+//       {aiResponse && aiResponse.choices && aiResponse.choices.length > 0 && (
+//         <div>
+//           <h2>AI Response:</h2>
+//           <p>{aiResponse.choices[0].content}</p> {/* Display the content of the first choice */}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default UserIntake;
--- server/ai_talking_points/ai_app/views.py
+++ server/ai_talking_points/ai_app/views.py
@@ -1,27 +1,41 @@
 from django.http import JsonResponse
+from ctransformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline, Conversation
 from django.views.decorators.csrf import csrf_exempt
 import json
-import bardapi
-import os
-import dotenv
+import torch
 
-dotenv.load_dotenv()
+# Load the model and tokenizer
+model_repo = "TheBloke/Kuchiki-L2-7B-GGUF"
+model = AutoModelForCausalLM.from_pretrained(model_repo, hf=True)
+tokenizer = AutoTokenizer.from_pretrained(model)
 
 @csrf_exempt
-def generate_talking_points(request):
+def generateTalkingPoints(request):
+
     if request.method == 'POST':
         try:
-            data = json.loads(request.body)
-            input_text = data.get('input_text', '')
+            # Get user input text from the POST request
+            request_data = json.loads(request.body.decode('utf-8'))
+            user_input = request_data.get('user_input', '')
 
-            if not input_text:
-                return JsonResponse({'error': 'Missing input_text in the request'}, status=400)
+            # Check if the user input is empty
+            if not user_input.strip():
+                return JsonResponse({'error': 'Empty user input'}, status=400)
 
-            token = os.environ['API_TOKEN']
-            response = bardapi.core.Bard(token).get_answer(input_text)
+            # Tokenize the user input with padding on the left side
+            input_ids = tokenizer.encode(user_input, return_tensors="pt", padding="max_length", max_length=100, truncation=True)
 
-            return JsonResponse({'aiResponse': response})
+            # Generate text using the model
+            with torch.no_grad():
+                output = model.generate(input_ids, max_length=100, num_return_sequences=1)
+
+            # Decode the generated text
+            generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+            return JsonResponse({'generated_text': generated_text})
+
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
-    else:
-        return JsonResponse({'error': 'Invalid request method'}, status=405)
+        
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
